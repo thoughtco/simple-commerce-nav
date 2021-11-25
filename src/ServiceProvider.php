5007f33ae426071f6335b929ab03e36f252495ea
Ryan Mitchell <ryan@thoughtcollective.com>
2021-11-25 13:07:28 +0000

    Bug fix and icon change

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -22,7 +22,7 @@ class ServiceProvider extends AddonServiceProvider
                 // add new ecommerce block
                 $nav->content('Ecommerce')
                     ->url($productsCollection->showUrl())
-                    ->icon('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">  <g transform="matrix(3.4285714285714284,0,0,3.4285714285714284,0,0)"><g>    <line x1="7" y1="4.5" x2="7" y2="3" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></line>    <g>      <path d="M5.5,8.5c0,.75.67,1,1.5,1s1.5,0,1.5-1c0-1.5-3-1.5-3-3,0-1,.67-1,1.5-1s1.5.38,1.5,1" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></path>      <line x1="7" y1="9.5" x2="7" y2="11" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></line>    </g>    <circle cx="7" cy="7" r="6.5" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></circle>  </g></g></svg>')
+                    ->icon('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">  <g transform="matrix(3.4285714285714284,0,0,3.4285714285714284,0,0)"><g>    <line x1="7" y1="4.5" x2="7" y2="3" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></line>    <g>      <path d="M5.5,8.5c0,.75.67,1,1.5,1s1.5,0,1.5-1c0-1.5-3-1.5-3-3,0-1,.67-1,1.5-1s1.5.38,1.5,1" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></path>      <line x1="7" y1="9.5" x2="7" y2="11" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></line>    </g>    <circle cx="7" cy="7" r="6.5" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></circle>  </g></g></svg>')
                     ->children([
                         
                         'Products' => $nav->item('Products')
@@ -42,7 +42,7 @@ class ServiceProvider extends AddonServiceProvider
                             ->can('view', $customersCollection),
                             
                     ])
-                    ->active('|products|coupons|orders|customers');
+                    ->active('xxx|products|coupons|orders|customers');
                 
                 // remove from 'collections'
                 $collections = $nav->content('Collections');
